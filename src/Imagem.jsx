1d648364195e2bcc5f40e8357354518f200edb52
Adriano Volter <91354299+AdrianoVolter@users.noreply.github.com>
2023-03-10 20:36:42 -0300

    Receita react V_1.1

--- src/Imagem.jsx
+++ src/Imagem.jsx
@@ -1,8 +1,16 @@
-function Imagem({ src, alt }) {
+import PropTypes from 'prop-types'; // ES6
+
+function Imagem(props) {
   return (
     <div>
-      <img src={src} alt={alt} />
+      <img src={props.src} alt={props.alt} />
     </div>
   );
+}
+
+
+Imagem.propTypes={
+  src:PropTypes.string.isRequired,
+  alt:PropTypes.string.isRequired,
 }
 export default Imagem;
